8fb61e59c54736d954399e57660becec34104a59
Vinicius Cerutti <vinicius.cerutti@acad.pucrs.br>
2017-11-13 16:11:06 -0200

    #1 (Aluno.java) Adicionado JML

--- ResultadoG1Aluno/src/Aluno.java
+++ ResultadoG1Aluno/src/Aluno.java
@@ -27,8 +27,23 @@ public class Aluno {
 	// ------------------------
 	// CONSTRUCTOR
 	// ------------------------
-
+	/*@
+	 @ private invariant p1 >= 0 && p1 <= 10;
+	 @ private invariant p2 >= 0 && p2 <= 10;
+	 @ private invariant t >= 0 && t <= 10;
+	 @ private invariant e >= 0 && e <= 10;
+	 @ private invariant g1 >= 0 && g1 <= 10;
+	 @ private invariant g2 >= 0 && g2 <= 10;
+	 @
+	 @ private invariant faltas>= 0 && faltas <= 60;
+	 @*/
+	
+	/*@
+	 @ requires aNome.contains(" ") && aNome.length() >=5 && aNome.length() <= 36;
+	 @ requires aMatricula.length() == 8;
+	 @*/
 	public Aluno(String aNome, String aMatricula, float aP1, float aP2, float aT, float aE, int aFaltas) {
+		aNome.length();
 		nome = aNome;
 		matricula = aMatricula;
 		p1 = aP1;
@@ -36,7 +51,7 @@ public class Aluno {
 		t = aT;
 		e = aE;
 		faltas = aFaltas;
-		resultado = GrauFinal.APR;
+		resultado = GrauFinal.REP;
 	}
 
 	// ------------------------
@@ -51,33 +66,33 @@ public class Aluno {
 		matricula = aMatricula;
 	}
 
-	public void setP1(float aP1) {
+	public /*@ pure helper @*/ void setP1(float aP1) {
 		p1 = aP1;
 	}
 
-	public void setP2(float aP2) {
+	public /*@ pure helper @*/ void setP2(float aP2) {
 		p2 = aP2;
 	}
 
-	public void setT(float aT) {
+	public /*@ pure helper @*/ void setT(float aT) {
 		t = aT;
 	}
 
-	public void setE(float aE) {
+	public /*@ pure helper @*/ void setE(float aE) {
 		e = aE;
 	}
 
-	public void setG2(float aG2) {
+	public /*@ pure helper @*/ void setG2(float aG2) {
 		DecimalFormat oneDForm = new DecimalFormat("#.#"); 
 
 		g2 = Float.valueOf(oneDForm.format(aG2));
 	}
 
-	public void setFaltas(int aFaltas) {
+	public /*@ pure helper @*/ void setFaltas(int aFaltas) {
 		faltas = aFaltas;
 	}
 
-	public void setG1(float aG1) {
+	public /*@ pure helper @*/  void setG1(float aG1) {
 		DecimalFormat oneDForm = new DecimalFormat("#.#"); 
 		g1 = Float.valueOf(oneDForm.format(aG1));
 	}
@@ -95,34 +110,43 @@ public class Aluno {
 		return matricula;
 	}
 
-	public float getP1() {
+	public /*@ pure helper @*/  float getP1() {
 		return p1;
 	}
 
-	public float getP2() {
+	public /*@ pure helper @*/ float  getP2() {
 		return p2;
 	}
 
-	public float getT() {
+	public /*@ pure helper @*/ float getT() {
 		return t;
 	}
 
-	public float getE() {
+	public /*@ pure helper @*/ float getE() {
 		return e;
 	}
 
-	public float getG2() {
+	public /*@ pure helper @*/ float getG2() {
 		return g2;
 	}
 
-	public int getFaltas() {
+	public /*@ pure helper @*/ int getFaltas() {
 		return faltas;
 	}
-
+	
+	/*@
+	 @ ensures \result == ((getP1()+getP2()+getT()+getE())/4); 
+	 @*/
 	public float getG1() {
 		return ((p1+p2+t+e)/4);
 	}
-
+	/*@
+	 @ ensures (getFaltas() > 15) ==> \result.equals(GrauFinal.REP);
+	 @ ensures (getG1() >= 7.0) ==> \result.equals(GrauFinal.APR);
+	 @ ensures (getG1() < 4.0) ==> \result.equals(GrauFinal.REP);
+	 @ ensures ((getG1()+getG2()) >= 5.0) ==> \result.equals(GrauFinal.APR);
+	 @ ensures \result.equals(GrauFinal.REP);
+	 @*/
 	public GrauFinal getResultado() {
 		if(faltas > 15) { resultado = GrauFinal.REP;}
 		else if(g1 >= 7.0) {resultado = GrauFinal.APR;}
